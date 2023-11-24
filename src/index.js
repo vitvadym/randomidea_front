409d0d51c5b7ef9072277046a1015cd155c94a80
Vadym <vadym_v@ukr.net>
2023-11-24 03:21:41 +0200

    add font awesome cdn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
+import './css/style.css';
+
 import Modal from './components/Modal';
 import IdeaForm from './components/IdeaForm';
 import IdeaList from './components/IdeaList';
-
-import '@fortawesome/fontawesome-free/css/all.css';
-import './css/style.css';
 
 new Modal();
 
